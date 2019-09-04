4923c1d68a31d59fa4c46250b268d47a15d80f7c
(Noah) Hanlun Song <hsong@air.org>
2019-09-04 17:20:37 -0400

    Not found response for single task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import * as uuid from 'uuid/v1';
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -28,7 +28,11 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+        if (!found) {
+            throw new NotFoundException(`task with ID ${id} not found`);
+        }
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
