ece307821d583deb02a10fe7edf9d1f78007549e
Noah <hanlun.song@gmail.com>
2019-09-04 19:35:54 -0400

    Remove interface model to use entity

--- src/tasks/dtos/get-tasks-filter.dto.ts
+++ src/tasks/dtos/get-tasks-filter.dto.ts
@@ -1,11 +1,11 @@
-import { TaskStatus } from '../task.model';
 import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
+import { TaskStatus } from '../task-status.enum';
 
 export class GetTasksFilterDto {
-    @IsOptional()
-    @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
-    status: TaskStatus;
-    @IsOptional()
-    @IsNotEmpty()
-    search: string;
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+  status: TaskStatus;
+  @IsOptional()
+  @IsNotEmpty()
+  search: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,44 +1,54 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { GetTasksFilterDto } from './dtos/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-        if (Object.keys(filterDto).length) {
-            return this.tasksService.getTasksWithFilters(filterDto);
-        } else {
-            return this.tasksService.getAllTasks();
-        }
-    }
+  // @Get()
+  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+  //     if (Object.keys(filterDto).length) {
+  //         return this.tasksService.getTasksWithFilters(filterDto);
+  //     } else {
+  //         return this.tasksService.getAllTasks();
+  //     }
+  // }
 
-    @Get('/:id')
-    getTaskById(@Param('id') id: string): Task {
-        return this.tasksService.getTaskById(id);
-    }
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string): Task {
+  //     return this.tasksService.getTaskById(id);
+  // }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.tasksService.createTask(createTaskDto);
-    }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //     return this.tasksService.createTask(createTaskDto);
+  // }
 
-    @Delete('/:id')
-    deleteTask(@Param('id') id: string): void {
-        this.tasksService.deleteTaskById(id);
-    }
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //     this.tasksService.deleteTaskById(id);
+  // }
 
-    @Patch('/:id/status')
-    updateTAskStatus(
-        @Param('id') id: string,
-        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-    ): Task {
-        return this.tasksService.updateTaskStatus(id, status);
-    }
+  // @Patch('/:id/status')
+  // updateTAskStatus(
+  //     @Param('id') id: string,
+  //     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  // ): Task {
+  //     return this.tasksService.updateTaskStatus(id, status);
+  // }
 }
