2113d6e643e9e910fb24f478b28730a44c7aac32
Noah <hanlun.song@gmail.com>
2019-09-04 20:48:03 -0400

    Refactor GET tasks wtih ORM

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,22 +12,11 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
   ) {}
-  // getAllTasks(): Task[] {
-  //     return this.tasks;
-  // }
-  // getTasksWithFilters(filterDto: GetTasksFilterDto) {
-  //     const { status, search } = filterDto;
-  //     let tasks = this.getAllTasks();
-  //     if (status) {
-  //         tasks = tasks.filter(task => task.status === status);
-  //     }
-  //     if (search) {
-  //         tasks = tasks.filter(
-  //             task => task.title.includes(search) || task.description.includes(search),
-  //         );
-  //     }
-  //     return tasks;
-  // }
+
+  getTasks(filterDto: GetTasksFilterDto) {
+    return this.taskRepository.getTasks(filterDto);
+  }
+
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
     if (!found) {
