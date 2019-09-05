6a0936a14a4d691b19c3520e394b77346d8477f6
(Noah) Hanlun Song <hsong@air.org>
2019-09-05 16:43:02 -0400

    Configuration set up for deployment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,22 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt-strategy';
+import * as config from 'config';
+
+interface JwtConfig {
+  secret: string;
+  expiresIn: number;
+}
+
+const jwtConfig: JwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
     JwtModule.register({
-      secret: 'VeryVerySecretive123',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: jwtConfig.expiresIn,
       },
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
